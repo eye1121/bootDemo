e09e58887f3145dc196b5bef65b771abb5cc9e6a
eye1121 <38984312+eye1121@users.noreply.github.com>
2020-07-08 16:55:33 +0800

    first commit

---
+++ src/main/java/com/cloudtogo/ClougtogoApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClougtogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClougtogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudtogo/ClougtogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClougtogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
